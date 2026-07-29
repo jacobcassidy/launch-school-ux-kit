11af0c8b0406b1061a41047cafd04703f5fb4094
Jacob Cassidy <jacob@jacobcassidy.com>
2026-07-29 09:43:55 +0700

    Update colorLog with oklch colors

--- src/js/modules/helpers/log.js
+++ src/js/modules/helpers/log.js
@@ -4,10 +4,10 @@
  */
 
 export const colorLog = {
-  alert: (msg, ...args) => console.log(`%c${msg}`, "color: #f00; font-weight: 700;", ...args),
-  debug: (msg, ...args) => console.log(`%c${msg}`, "color: #f80;", ...args),
-  detail: (msg, ...args) => console.log(`%c${msg}`, "color: #999;", ...args),
-  info: (msg, ...args) => console.log(`%c${msg}`, "color: #007acc;", ...args),
-  notice: (msg, ...args) => console.log(`%c${msg}`, "color: #f08; font-weight: 700;", ...args),
-  run: (msg, ...args) => console.log(`%c${msg}`, "color: #28a745;", ...args),
+  alert: (msg, ...args) => console.log(`%c${msg}`, "color: oklch(0.7 0.15 30); font-weight: 700;", ...args),
+  debug: (msg, ...args) => console.log(`%c${msg}`, "color: oklch(0.7 0.15 60);", ...args),
+  detail: (msg, ...args) => console.log(`%c${msg}`, "color: oklch(0.7 0 0);", ...args),
+  info: (msg, ...args) => console.log(`%c${msg}`, "color: oklch(0.7 0.15 250);", ...args),
+  notice: (msg, ...args) => console.log(`%c${msg}`, "color: oklch(0.7 0.15 0);", ...args),
+  run: (msg, ...args) => console.log(`%c${msg}`, "color: oklch(0.7 0.15 150);", ...args),
 };
