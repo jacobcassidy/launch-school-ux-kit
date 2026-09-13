5571e94682e160fd93e242c36c34434a6edbe3ba
Jacob Cassidy <jacob@jacobcassidy.com>
2026-09-14 05:25:07 +0700

    Add `.hide-single-count` back

--- src/js/modules/components/sidebar.js
+++ src/js/modules/components/sidebar.js
@@ -251,6 +251,8 @@ function removeCountParentheses() {
       newCountEl.className = "unread-count";
       newCountEl.textContent = countText.replace(/[()]/g, "");
 
+      if (newCountEl.textContent === "1") newCountEl.classList.add("hide-single-count");
+
       count.after(newCountEl);
     };
 
