5600ec4bdfbfd5d7db52e3f0aaf636792051af97
Jacob Cassidy <jacob@jacobcassidy.com>
2026-09-18 16:07:16 +0700

    Update includeIgnoreFile import

--- eslint.config.js
+++ eslint.config.js
@@ -9,16 +9,15 @@
 
 import globals from "globals";
 import js from "@eslint/js";
-import { defineConfig, globalIgnores } from "eslint/config";
+import { defineConfig, globalIgnores, includeIgnoreFile } from "eslint/config";
 import { fileURLToPath } from "node:url";
 import { importX } from "eslint-plugin-import-x";
-import { includeIgnoreFile } from "@eslint/compat";
 
 const gitignorePath = fileURLToPath(new URL(".gitignore", import.meta.url));
 
 export default defineConfig([
-  globalIgnores(["**/*.min.js"]),
   includeIgnoreFile(gitignorePath, "Imported .gitignore patterns"),
+  globalIgnores(["**/*.min.js"]),
   {
     plugins: {
       "import-x": importX,
